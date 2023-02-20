8c62fa2dae4737e18eb5b185c49e5b4c6b8d6bf9
lauralardies <lrodrlar@myuax.com>
2023-02-20 17:11:51 +0100

    descargar en modo asíncrono

--- prueba.py
+++ prueba.py
@@ -1,6 +1,25 @@
 import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from os import sep
+
+async def wget(session, uri):
+    async with session.get(uri) as response:
+        if response.status != 200:
+            return None
+        if response.content_type.startswith('text/'):
+            return await response.text()
+        else:
+            return await response.read()
+
+async def download(session, uri):
+    content = await wget(session, uri)
+    if content is None:
+        return None
+    with open(uri.split(sep)[-1], 'wb') as f:
+        f.write(content)
+        return uri
 
 def get_images_src_from_html(html_doc):
     '''
@@ -28,3 +47,16 @@ def get_uri_from_images_src(base_uri, images_src):
             yield parsed_base.scheme + '://' + parsed_base.netloc + path
         else:
             yield parsed.geturl()
+
+async def main(uri):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(uri) as response:
+            if response.status != 200:
+                return None
+            if response.content_type.startswith('text/'):
+                return await response.text()
+            else:
+                return await response.read()
+
+
+asyncio.run(main("http://www.formation-python.com/")) 
