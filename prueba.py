79ffb44336f4b5f3f33ff3763eaa4d0dd5649e10
lauralardies <lrodrlar@myuax.com>
2023-02-20 16:48:28 +0100

    codigo tarea

---
+++ prueba.py
@@ -0,0 +1,30 @@
+import asyncio
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+def get_images_src_from_html(html_doc):
+    '''
+    Recupera todo el contenido de los atributos src de las etiquetas img.
+    '''
+    soup = BeautifulSoup(html_doc, 'html_parser')
+    return (img.get('scr') for img in soup.find_all('img'))
+
+def get_uri_from_images_src(base_uri, images_src):
+    '''
+    Devuelve una a una cada URI de la imagen a descargar.
+    '''
+    parsed_base = urlparse(base_uri)
+    for src in images_src:
+        parsed = urlparse(src)
+        if parsed.netloc == '':
+            path = parsed.path
+            if parsed.query:
+                path += '?' + parsed.query
+            if path[0] != '/':
+                if parsed_base.path == '/':
+                    path = '/' + path
+                else:
+                    path = '/' + '/'.join(parsed_base.path.split('/')[:-1]) + '/' + path
+            yield parsed_base.scheme + '://' + parsed_base.netloc + path
+        else:
+            yield parsed.geturl()
